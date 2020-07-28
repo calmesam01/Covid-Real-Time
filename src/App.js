e4a4f79cc222e47dbf1ffb134f5a42f01707608f
calmesam01 <sanyamsareen01@gmail.com>
2020-07-28 03:07:34 -0400

    Display the JSON data using cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Card from "./components/Card";
 import { Button } from "reactstrap";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
 
@@ -24,20 +24,22 @@ function App() {
   useEffect(() => {
     fetch(`https://corona.lmao.ninja/v2/countries/${country}?yesterday=true&strict=true&query`)
       .then(response => response.json())
-      .then(res => changeRes(res))
+      .then(resp => changeRes(resp))
       .catch(err => console.log(err)
-      )
+      );
   }, [{clickFalse}]);
 
   return (
     <div className="App">
-      <div style={{ margin: 100 }}>
+    <strong><h1 style={{ textAlign: "center", marginBottom: 50, marginLeft: 100, marginRight: 100, marginTop: 50 }}>Real Time Corona Meter</h1></strong>
+      <div style={{ marginBottom: 50, marginLeft: 100, marginRight: 100, marginTop: 50 }}>
+
         <form style={{ width: window }}>
           <input type="text" onChange={handleChange} name="searchBox" placeholder="Type in the country name" className="searchBox form-control" style={{ marginBottom: 20 }}></input>
           <Button type="submit" onClick={onBtnClick} className="btn btn-md btn-success form-control" style={{ width: window }}>Search</Button>
         </form>
       </div>
-      <Card cases={res["cases"]} country={res["country"]} active={res["active"]} />
+      <Card cases={res["cases"]} population={res["population"]} tests={res["tests"]} country={res["country"]} active={res["active"]} recovered={res["recovered"]} deaths={res["deaths"]} todayRecovered={res["todayRecovered"]} todayDeaths={res["todayDeaths"]} todayCases={res["todayCases"]} />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,30 @@ import React from "react";
 
 function Card (props) {
     return (
-        <div className = "card" style = {{margin: 100, textAlign: "center"}}>
-            <img src = "https://picsum.photos/200/200" style = {{width: 200, height: 200}} className = "card-img-top"></img>
+        <div className = "card" style = {{margin: 100, marginTop: 10, textAlign: "center"}}>
             <div className = "card-body">
                 <div className = "card-title">
-                    {props.country}
-                </div>
+                    <h2><strong>{props.country}</strong></h2>
+                </div><br/>
                 <div className = "card-text">
-                <h3>Total number of cases: </h3> {props.cases}
-                <h3>Number of active cases: </h3> {props.active}
+                <div className="row">
+                    <div className="col-md-4">
+                        <h3>Population </h3> {props.population}
+                        <h3>Number of tests </h3> {props.tests}
+                        <h3>Total number of cases </h3> {props.cases}
+                    </div>
+                    <div className="col-md-4">
+                        <h3>Active </h3> {props.active}  
+                        <h3>Recovered </h3> {props.recovered}  
+                        <h3>Deaths </h3> {props.deaths}  
+                    </div>
+                    <div className="col-md-4">
+                        <h3>New cases today </h3> {props.todayCases}
+                        <h3>Recovered today </h3> {props.todayRecovered}  
+                        <h3>Deaths today </h3> {props.todayDeaths}  
+                    </div>
+                </div>
+                
                 </div>
             </div>
         </div>
